14523ef931ac15b1d1a9e858955f5a58c17bfc62
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-11-11 23:13:22 +0200

    fix

--- contacts.js
+++ contacts.js
@@ -1,95 +1,80 @@
 const fs = require('fs').promises;
 const path = require('path');
 require('colors');
-const idGenerator = require('./utils/utils');
+const { v4: uuidv4 } = require('uuid');
 
 const contactsPath = path.join(__dirname + '/db/contacts.json');
 
+async function getContacts() {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+}
+
 async function listContacts() {
-    try {
-        const data = await fs.readFile(contactsPath);
-        const contacts = await JSON.parse(data.toString());
-
-        if (contacts.length === 0) {
-            console.log('Your contact-list is empty'.red);
-            return;
-        }
-
-        console.table(contacts);
-    } catch (error) {
-        console.log(error);
+    const contacts = await getContacts();
+    
+    if (contacts.length === 0) {
+        console.log('Your contact-list is empty'.red);
+        return;
     }
+
+    console.table(contacts);
 }
 
 async function getContactById(contactId) {
-    try {
-        const data = await fs.readFile(contactsPath);
-        const contacts = await JSON.parse(data.toString());
+    const contacts = await getContacts();
 
-        if (isNaN(contactId)) {
-            console.log(`id format is incorrect`.red);
-            return;
-        }
-
-        const contact = contacts.find(({ id }) => id === contactId);
-
-        if (!contact) {
-            console.log(`Contact with id=${contactId} not found`.red);
-            return;
-        }
-
-        console.table([contact])
-    } catch (error) {
-        console.log(error);
+    if (isNaN(contactId)) {
+        console.log(`id format is incorrect`.red);
+        return;
     }
+
+    const contact = contacts.find(({ id }) => id === contactId);
+
+    if (!contact) {
+        console.log(`Contact with id=${contactId} not found`.red);
+        return;
+    }
+
+    console.table([contact])
 }
 
 async function removeContact(contactId) {
-    try {
-        const data = await fs.readFile(contactsPath);
-        const contacts = await JSON.parse(data.toString());
+    const contacts = await getContacts();
 
-        if (isNaN(contactId)) {
-            console.log(`id format is incorrect`.red);
-            return;
-        }
-
-        const filteredContacts = contacts.filter(({ id }) => id !== contactId);
-
-        if (contacts.length === filteredContacts.length) {
-            console.log(`Contact with id=${contactId} not found`.red);
-            return;
-        }
-
-        await fs.writeFile(contactsPath, JSON.stringify(filteredContacts));
-        console.log(`Contact with id=${contactId} successfully removed`.green);
-    } catch (error) {
-        console.log(error);
+    if (isNaN(contactId)) {
+        console.log(`id format is incorrect`.red);
+        return;
     }
+
+    const filteredContacts = contacts.filter(({ id }) => id !== contactId);
+
+    if (contacts.length === filteredContacts.length) {
+        console.log(`Contact with id=${contactId} not found`.red);
+        return;
+    }
+
+    await fs.writeFile(contactsPath, JSON.stringify(filteredContacts));
+    console.log(`Contact with id=${contactId} successfully removed`.green);
 }
 
 async function addContact(name, email, phone) {
-    try {
-        const data = await fs.readFile(contactsPath);
-        const contacts = await JSON.parse(data.toString());
-        
-        if (!name || !email || !phone) {
-            console.log('not entered all required arguments'.red);
-            return;
-        }
-
-        contacts.push({
-            id: idGenerator(contacts),
-            name,
-            email,
-            phone
-        });
-
-        await fs.writeFile(contactsPath, JSON.stringify(contacts));
-        console.log(`New contact successfully added`.green);
-    } catch (error) {
-        console.log(error);
+    const contacts = await getContacts();
+    
+    if (!name || !email || !phone) {
+        console.log('not entered all required arguments'.red);
+        return;
     }
+
+    contacts.push({
+        id: uuidv4(),
+        name,
+        email,
+        phone
+    });
+
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    console.log(`New contact successfully added`.green);
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -2,25 +2,29 @@ const contactsApi = require('./contacts');
 const argv = require('yargs').argv;
 
 function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case 'list':
-      contactsApi.listContacts();
-      break;
+  try {
+    switch (action) {
+      case 'list':
+        contactsApi.listContacts();
+        break;
 
-    case 'get':
-      contactsApi.getContactById(id);
-      break;
+      case 'get':
+        contactsApi.getContactById(id);
+        break;
 
-    case 'add':
-      contactsApi.addContact(name, email, phone);
-      break;
+      case 'add':
+        contactsApi.addContact(name, email, phone);
+        break;
 
-    case 'remove':
-      contactsApi.removeContact(id);
-      break;
+      case 'remove':
+        contactsApi.removeContact(id);
+        break;
 
-    default:
-      console.warn('\x1B[31m Unknown action type!');
+      default:
+        console.warn('\x1B[31m Unknown action type!');
+    }    
+  } catch (error) {
+    console.error(error);
   }
 }
 
